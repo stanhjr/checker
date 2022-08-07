d7c741d372b74fa8d76124b08083c70886bf15bc
stanhjr <stanislav.o@cybearsoft.com>
2022-08-07 19:21:14 +0300

    fix gmail checker

--- email_checkers/gmail_checker.py
+++ email_checkers/gmail_checker.py
@@ -11,9 +11,8 @@ def gmail_checker(email) -> bool:
     req = requests.Session()
     request = req.get('https://mail.google.com/mail/gxlu?email=' + email)
 
-    if len(request.cookies) > 0:
-        return False
-    return True
+    if len(request.cookies) == 0:
+        return True
 
 
 

81b323ab47ffd05d88ac2b7673bbf82a5e9d8b0f
stanhjr <stanislav.o@cybearsoft.com>
2022-09-08 12:34:44 +0300

    remove async celery

--- tools.py
+++ tools.py
@@ -30,7 +30,7 @@ def start_checker_email():
             email = email.replace("\n", "")
             email = email.replace("\t", "")
             email = email.replace(" ", "")
-            check_email.apply_async(kwargs=dict(email=email))
+            check_email.delay(email=email)
             if not email:
                 break
 

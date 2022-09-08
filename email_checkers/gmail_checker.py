760a476b336679f1e3e59237ab3b637a95efcbbc
stanhjr <stanislav.o@cybearsoft.com>
2022-09-08 10:44:32 +0300

    email checker

---
+++ email_checkers/gmail_checker.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def gmail_checker(email) -> bool:
+    req = requests.Session()
+    request = req.get('https://mail.google.com/mail/gxlu?email=' + email)
+
+    if len(request.cookies) > 0:
+        return True
+
+
+
+
